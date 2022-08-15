e0e906d3c177dcbb7a1b98b6568337ea62b36c28
Jennie Duffer <jenniej297@gmail.com>
2022-08-15 14:49:14 -0500

    initial commit

---
+++ MethodOverloadingExercise/Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExercise
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add( decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+            if (isTrue)
+            {
+                sum = a + b;
+
+                if (sum == 1)
+                    response = $"{sum} dollar";
+
+                else
+                {
+                    response = $"{sum} dollars";
+                }
+            }
+            return response;
+        }
+           
+
+    }
+}
---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using MethodOverloadingExercise;
+
+Console.WriteLine(Methods.Add(2, 3, true));
+Console.WriteLine();
+
+Console.WriteLine(Methods.Add(7, 2, true));
+Console.WriteLine();
+
+Console.WriteLine(Methods.Add(-5, 6, true));
+Console.WriteLine();
